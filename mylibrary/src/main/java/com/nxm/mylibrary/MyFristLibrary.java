e38561c826d7a6891ebb00ab41e5860a45b17fa8
muzi102 <1565675791@qq.com>
2018-10-10 22:07:08 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/nxm/mylibrary/MyFristLibrary.java
@@ -0,0 +1,15 @@
+package com.nxm.mylibrary;
+
+/**
+ * **************************************************************************************************
+ * 修改日期                         修改人             任务名称                         功能或Bug描述
+ * 2018/10/10 22:05                  MUZI102
+ * **************************************************************************************************
+ */
+public class MyFristLibrary {
+    private String name = "MUZI102";
+
+    public String getString(String conent) {
+        return name + conent;
+    }
+}
